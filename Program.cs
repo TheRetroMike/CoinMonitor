8cb4753ab9bb804003788360146b46641b59eebe
RetroMike <michaeldlesk@gmail.com>
2024-09-18 22:42:49 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,15 @@
-﻿using CoinReleaseMonitor;
+using CoinReleaseMonitor;
 
-//var args = Environment.GetCommandLineArgs();
 if (args.Length == 0)
 {
     Console.WriteLine("Must Specify Discord Webhook Url");
 }
 else
 {
-    Console.WriteLine("Using Discord Webhook Url: " + args[0]);
-    var startTimeSpan = TimeSpan.Zero;
-    var periodTimeSpan = TimeSpan.FromMinutes(15);
-    Processor.Process(args[0]);
-    var timer = new System.Threading.Timer((e) =>
+    while (true)
     {
         Processor.Process(args[0]);
-    }, null, startTimeSpan, periodTimeSpan);
+        Thread.Sleep(TimeSpan.FromMinutes(15));
+    }
 }
 Console.ReadLine();
